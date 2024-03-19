8d1a48f6346ebe24ba436ebdb101e1fae47892fc
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-03-20 04:51:41 +0600

    uploaaadinngggniggga

---
+++ app/(root)/(routes)/settings/page.tsx
@@ -0,0 +1,8 @@
+import { SubscriptionButton } from "@/components/subscription-button";
+
+
+const SettingsPage = async () => {
+  
+}
+ 
+export default SettingsPage;
---
+++ scripts/seed.ts
@@ -0,0 +1,21 @@
+const { PrismaClient } = require('@prisma/client');
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: 'Government Side' },
+        { name: 'Opposition Side' },
+        { name: 'Coding Mentor' },
+      ],
+    });
+  } catch (error) {
+    console.error('Error seeding default categories:', error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
