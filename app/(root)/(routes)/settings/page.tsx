5360d29aaea978f56379d42b162d658fa1ab5c8e
Hasin Raiyan <fullcopyrightsounds@gmail.com>
2024-03-20 05:11:50 +0600

    Update page.tsx

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const NormalPage: React.FC = () => {
+  return (
+    <div>
+      <h1>This is a Normal Page</h1>
+      <p>Welcome to my normal page!</p>
+    </div>
+  );
+};
+
+export default NormalPage;
